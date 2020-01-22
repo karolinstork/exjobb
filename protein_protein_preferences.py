4b95ee867f661c30d24e26486c0139a81f7fa0e2
karolinstork <59958427+karolinstork@users.noreply.github.com>
2020-01-22 15:05:19 +0100

    börjat bygga ett skal

---
+++ protein_protein_preferences.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+
+
+
+
+
+
+
+
+
+
+def main():
+
+    args=sys.argv[1:]
+
+    #try:
+    #    pdb_files_list=args[0]
+    #except IndexError:
+    #    print "ERROR: You must add a pdb list"
+
+
+
+
+if __name__ == '__main__':
+  main()
