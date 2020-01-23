4f4b865a877e713a7399b6d3976ca9365939542f
Karolin Stork <karst825@student.liu.se>
2020-01-23 14:54:23 +0100

    not much new, waiting on access to input material

--- protein_protein_preferences.py
+++ protein_protein_preferences.py
@@ -3,6 +3,9 @@ import sys
 
 
 
+#def calc_distances(pdb_filename):
+    #c-script
+
 
 
 
@@ -13,13 +16,11 @@ import sys
 def main():
 
     args=sys.argv[1:]
+    #pdb_filename=args[0]
 
-    #try:
-    #    pdb_files_list=args[0]
-    #except IndexError:
-    #    print "ERROR: You must add a pdb list"
 
-    print ("didnt work, i will try again")
+
+
 
 
 if __name__ == '__main__':
