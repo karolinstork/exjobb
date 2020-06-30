82512a579ae4ba16229d56420231fc12278bac08
Karolin Stork <x_karst@tetralith2.nsc.liu.se>
2020-06-30 15:24:03 +0200

    fixed find_all_contacts and added a function for plot of interface composition

--- run_C_script.py
+++ run_C_script.py
@@ -14,7 +14,11 @@ import seaborn as sns
 
 
 
-def calculate_contacts(complex, res_dict):
+
+
+def find_all_contacts(complex, res_dict, composition_dict):
+    translation_dict = {}
+
     line = complex.strip('\n')
     files = line.split('\t')
     file_1 = files[0]
@@ -34,8 +38,9 @@ def calculate_contacts(complex, res_dict):
         only_1_model = True
 
     if only_1_model:
+        complex = complex.strip('\n')
+        print("----------------", complex, "------------------")
 
-        #print(base_file, file_1, file_2, chain_1, chain_2)
         output_file = open(f"/proj/wallner/users/x_karst/exjobb/c_script_output/{pdb_id}.output", "w")
         dir = base_file[1:3]
         target = f'/proj/wallner/share/PDB/191015_biounit/{dir}/{base_file}'
@@ -43,9 +48,19 @@ def calculate_contacts(complex, res_dict):
         output_file.close()
 
         output_read = open(f"/proj/wallner/users/x_karst/exjobb/c_script_output/{pdb_id}.output", "r")
-        #lines = output_read.readlines()
+
         text = output_read.read()
         lines = text.split('\n')
+
+        for line in lines: #translation_dict
+            if line[:3] == "RES":
+                line_split_by_space = line.split()
+                pos = (line_split_by_space[2])
+                res = (line_split_by_space[3])
+                chain = (line_split_by_space[4]).strip(":")
+                pos_chain = pos+chain
+                translation_dict[pos_chain] = res
+
 
 
         for line in lines:
@@ -54,56 +69,94 @@ def calculate_contacts(complex, res_dict):
                 res = (line_split_by_space[3])
                 chain = (line_split_by_space[4]).strip(":")
                 #print(res, chain, chain_1[:-1], chain_2[:-1])
-                #print(line)
 
-                if chain == chain_1[:-1]:       #only interesed in contacts between 2 chains not in the same chain
-                    pattern = f"[\d]+{chain_2[:-1]}"
-                    contacts = re.findall(pattern, line)
-                    if len(contacts)>0:
-                        #print(contacts)
-                        for contact in contacts:    #find out what residue the contacts are
-                            contact_chain = contact[-1:]
-                            contact_pos = contact[:-1]
-                            #print(contact_chain, contact_pos)
-                            pattern1 = f'RES[\s]+[\d]+[\s]+{contact_pos}[\s]+([\w])[\s]+{contact_chain}:'
-                            #print(pattern1)
-                            match = re.search(pattern1, text)
-                            if match:
-                                #print("REAL AMINO ACID IS: ", match.group(1))
-                                res_dict[res].update(match.group(1))
+                pattern = f":(.*)"
+                all_contacts_in_string = re.findall(pattern, line)
 
+                if len(all_contacts_in_string)>1:
+                    print("ERROR")
+                    return
 
+                list_of_contacts = all_contacts_in_string[0].split()
+                if len(list_of_contacts)>0:
+                    for contact in list_of_contacts:
+                        if contact[-1:] != chain:  #only interested in contacts between 2 chains not in the same chain
+                            contact_residue = translation_dict[contact]
+                            res_dict[res].update(contact_residue)
+                            res_dict[contact_residue].update(res)
 
-                        #res_dict[res].append(contacts)
-
-                if chain == chain_2[:-1]:
-                    pattern = f"[\d]+{chain_1[:-1]}"
-                    contacts = re.findall(pattern, line)
-                    if len(contacts)>0:
-                        #print(contacts)
-                        for contact in contacts:    #find out what residue the contacts are
-                            contact_chain = contact[-1:]
-                            contact_pos = contact[:-1]
-                            #print(contact_chain, contact_pos)
-                            pattern2 = f'RES[\s]+[\d]+[\s]+{contact_pos}[\s]+([\w])[\s]+{contact_chain}:'
-                            #print(pattern1)
-                            match = re.search(pattern2, text)
-                            if match:
-                                #print("REAL AMINO ACID IS: ", match.group(1))
-                                res_dict[res].update(match.group(1))
+                            composition_dict[res].update(contact_residue)
+                            composition_dict[contact_residue].update(res)
+                            #print("Inter-chain contact with ",res ,"found:", contact, "=", contact_residue)
 
 
     return
 
 
 
-def heatmap(res_dict):
-    #x_axis = list(res_dict.keys())
+
+
+
+
+def calc_composition(composition_dict, check):
+    tot_interactions = 0
+    number_of_contacts_dict = {}
+    percentages_dict = {}
+    plot_list = []
+    x_list = []
+    y_list = []
+
+    for key, value in composition_dict.items():
+        list_of_contacts = list(value.elements())
+        number_of_contacts_dict[key] = len(list_of_contacts)
+        tot_interactions = tot_interactions + len(list_of_contacts)
+
+    print("Total number of interactions: ", tot_interactions)
+    print("Prevalence of residue in interactions:")
+    for residue, interactions in number_of_contacts_dict.items():
+        ratio = interactions/tot_interactions
+        ratio = (ratio*100)
+        ratio = round(ratio, 2)
+        print(residue+":", ratio, "%")
+        percentages_dict[residue] = ratio
+        plot_list.append((residue, ratio))
+
+
+    plot_list = sorted(plot_list, key = lambda x:x[1], reverse = True)
+    for item in plot_list:
+        x_list.append(item[0])
+        y_list.append(item[1])
+
+
+
+    # df = pd.DataFrame(plot_list)
+    # df = df.transpose()
+    # print(df)
+    # percent_plot = sns.scatterplot(data = df)
+
+
+
+    import matplotlib.pyplot as plt
+    plt.scatter(x_list, y_list)
+    plt.plot(x_list, y_list)
+    plt.xlabel("Residues")
+    plt.ylabel("Wi [%]")
+    title = "Residue composition for " + str(check)+ " complexes"
+    plt.title(title)
+    plt.show()
+
+
+    return
+
+
+
+def plot_heatmap(res_dict, check):
     df = pd.DataFrame(res_dict)
     df = df.sort_index(0, ascending=False)
     df = df.sort_index(1)
     print(df)
-    map = sns.heatmap(df, linewidth = 0.5, cmap="YlGnBu")
+    title = "Number of interactions in "+ str(check)+ " complexes"
+    heatmap = sns.heatmap(df, linewidth = 0.5, cmap="YlGnBu").set_title(title)
 
     import matplotlib.pyplot as plt
     plt.show()
@@ -111,19 +164,6 @@ def heatmap(res_dict):
     return
 
 
-def calc_composition(res_dict):
-    tot_interactions = 0
-    for key, value in res_dict.items():
-        list_of_contacts = list(value.elements())
-        print("Residue", key, "has contact with", len(list_of_contacts), "residues")
-
-        tot_interactions = tot_interactions + len(list_of_contacts)
-
-    print("Total number of interactions in this complex: ", tot_interactions)
-
-
-
-    return
 
 
 
@@ -136,25 +176,25 @@ def main():
     complexes = dataset_file.readlines()
 
     res_dict = defaultdict(Counter)
+    composition_dict = defaultdict(Counter)
 
     check = 0
+
     for complex in complexes:
-        calculate_contacts(complex, res_dict)
+        try:
+            find_all_contacts(complex, res_dict, composition_dict)
+        except KeyError:
+            print("KeyError in ", complex)
+
+
+
         check = check + 1
-        calc_composition(res_dict)
-        print("File ", check, "complete")
-        if check == 10:
+        if check == 621:
             print(res_dict)
-            heatmap(res_dict)
+            plot_heatmap(res_dict, check)
+            calc_composition(composition_dict, check)
 
             return
-
-
-
-
-
-
-
 
 
 
